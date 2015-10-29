2f85010423a577eb6373040865a1e8532051428c
miya-masa <miyauchi0623@gmail.com>
2015-10-29 23:53:54 +0900

    一部実装修正

--- src/main/java/com/miya_masa/sample/controller/BasicController.java
+++ src/main/java/com/miya_masa/sample/controller/BasicController.java
@@ -3,14 +3,14 @@ package com.miya_masa.sample.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("userView")
 @Controller
-public class UserViewController {
+@RequestMapping("basic")
+public class BasicController {
 
 	@RequestMapping
 	public String index() {
 
-		return "index";
+		return "basic";
 	}
 
 }
